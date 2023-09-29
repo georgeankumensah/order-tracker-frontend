9774d1d0c2dc175f0f4d7e77d6290edf7a59d8a8
georgeankumensah <georgeankumensah@gmail.com>
2023-09-29 05:03:45 -0700

    added timeline

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -4,66 +4,58 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { selectedProduct } from "../redux/actions/productActions";
 import useClipboard from "react-use-clipboard";
-import {AiOutlineCopy} from "react-icons/ai"
+import { AiOutlineCopy } from "react-icons/ai";
 import { Chrono } from "react-chrono";
 
-
-import spinner from "../assets/spinner.gif"
-
-
+import spinner from "../assets/spinner.gif";
 
 const ProductDetail = () => {
   const { shareableId } = useParams();
-  const orderStatus =[
-    {
-      title: 'In Transit',
-      cardTitle: 'First Stage',
-      cardSubtitle: 'Subtitle 1',
-      cardDetailedText: 'Description for Stage 1',
-    },
-    {
-      title: 'In Transport',
-      cardTitle: 'Second Stage',
-      cardSubtitle: 'Subtitle 2',
-      cardDetailedText: 'Description for Stage 2',
-    },
-    {
-      title: 'Available for pickup',
-      cardTitle: 'Third Stage',
-      cardSubtitle: 'Subtitle 3',
-      cardDetailedText: 'Description for Stage 3',
-    },
-  ];
+  const [fetchedStatus, setFetchedStatus] = useState([]);
+  const orderStatus = fetchedStatus;
   const [isCopied, setCopied] = useClipboard(shareableId, {
-    // `isCopied` will go back to `false` after 1000ms.
     successDuration: 1000,
-  });  const [isLoading, setIsLoading] = useState(false);
+  });
+
+  // yTknS4TqO
+  const [isLoading, setIsLoading] = useState(false);
   const url = "https://goldordertracker.onrender.com";
   const dispatch = useDispatch();
   const order = useSelector((state) => state.product);
-  const { name ,destination,image,_id,qty,status,warehouses,otherInformation,details} = order;
+  const {
+    name,
+    destination,
+    image,
+    _id,
+    qty,
+    status,
+
+    otherInformation,
+    details,
+  } = order;
+
   console.log(order);
   console.log(shareableId);
 
-  const getStatus = () =>{
-    const statusIndex = orderStatus.findIndex((item)=>
-      item.title === status)
-    return statusIndex
-  }
+  console.log(orderStatus);
 
-  const Loader = () =>{
-    return(
+  // const getStatus = () => {
+  //   const statusIndex = orderStatus.findIndex((item) => item.title === status);
+  //   return statusIndex;
+  // };
+
+  const Loader = () => {
+    setFetchedStatus(status);
+    return (
       <div className="flex items-center  ">
         <img src={spinner} className="w-[2rem] aspect-square" alt="spinner" />
-        <p className="font-medium ml-2">
-        Updating package details..
-        </p>
+        <p className="font-medium ml-2">Updating package details..</p>
       </div>
-    )
-  }
+    );
+  };
 
   const fetchOrderDetails = async () => {
-    setIsLoading(true)
+    setIsLoading(true);
     await axios
       .get(url + "/orders/shareable/" + shareableId)
       .then((res) => {
@@ -80,50 +72,78 @@ const ProductDetail = () => {
   };
 
   useEffect(() => {
-    if(shareableId && shareableId != "")fetchOrderDetails();
-    console.log("effect");
+    if (shareableId && shareableId != "") fetchOrderDetails();
+    console.log("updated data");
   }, [shareableId]);
 
   return (
     <div className="mx-[6rem] mt-[2rem]">
-        {isLoading  && <Loader/>}
-      <h1 className="text-3xl text-[#0a0a0a] font-medium">Order ID : {_id}</h1>
-      <div className="flex items-center gap-[1rem]">
-
-      <p className="font-medium text-[#0a0a0a]">Order date : </p>
-      <p className=" font-medium text-[#0a0a0a]">Estimated delivery : </p>
+      {isLoading && <Loader />}
+      <div className="flex justify-between">
+        <div>
+          <h1 className="text-3xl text-[#0a0a0a] font-medium">
+            Order ID : {_id}
+          </h1>
+          <div className="flex items-center gap-[1rem]">
+            <p className="font-medium text-[#0a0a0a]">Order date : </p>
+            <p className=" font-medium text-[#0a0a0a]">Estimated delivery : </p>
+          </div>
+        </div>
+        <button onClick={setCopied} className="font-medium">
+          Tracking ID :{" "}
+          {isCopied ? (
+            <span className="rounded-sm p-1 bg-green-400 text-white">
+              Copied <AiOutlineCopy className="inline-block ml-2" />
+            </span>
+          ) : (
+            <span className="rounded-sm p-1 bg-[#aaa7a7] text-white">
+              {shareableId}
+              <AiOutlineCopy className="inline-block ml-2" />
+            </span>
+          )}
+        </button>
       </div>
-      <hr className="my-[2rem]"/>
-     {!isLoading && <>
-      <div className="flex  items-start gap-[2rem]">
-      <img className="w-[20rem] aspect-auto rounded-md" src={image} alt={name} />
-      <div>
-      <div style={{ width: "800px" }}>
-        <Chrono activeItemIndex={getStatus()} lineWidth="5"  disableClickOnCircle  theme={{
-    primary: 'orange',
-    secondary: 'green',
-    cardBgColor: 'yellow',
-    titleColor: 'black',
-    titleColorActive: 'white',
-  }} items={orderStatus} mode="HORIZONTAL" cardLess hideControls />
-      </div>
-      <button onClick={setCopied} className="font-medium">
-      Tracking ID : {isCopied ? <span className="rounded-sm p-1 bg-green-400 text-white">Copied <AiOutlineCopy className="inline-block ml-2"/></span> :<span className="rounded-sm p-1 bg-[#aaa7a7] text-white">{shareableId}<AiOutlineCopy className="inline-block ml-2"/></span>}
-    </button>
+      <hr className="my-[2rem]" />
+      {!isLoading && (
+        <>
+          <div className="flex  items-start gap-[2rem]">
+            <img
+              className="w-[20rem] aspect-auto rounded-md"
+              src={image}
+              alt={name}
+            />
+            <div>
+                <ul className="steps">
+                  <li className="step step-primary">Order Confirmed</li>
+                  <li className="step step-primary">Shipped</li>
+                  <li className="step">Out For Delivery</li>
+                  <li className="step">Delivered</li>
+                </ul>
 
-      <br />
-      <p className="font-medium text-[#0a0a0a]">Package Name : <br /> {name}</p>
-      {destination && <><p>{destination.to}</p>
-      <p>{destination.from}</p></>}
-      <br />
-      <p className="font-medium text-[#0a0a0a]">Package Contains : <br />{details}</p>
-      <br />
-      <p className="font-medium text-[#0a0a0a]">Quantity : {qty}</p>
-      <p className="font-medium text-[#0a0a0a]">Status : {status}</p>
-      {/* <p>{warehouses}</p> */}
-      </div>
-
-      </div></>}
+              <br />
+              <p className="font-medium relative  text-[#0a0a0a] border-2 border-red-600">
+                Package Name : <br />{" "}
+                <span className="font-normal text-xl">{name}</span>{" "}
+              </p>
+              {destination && (
+                <>
+                  <p>{destination.to}</p>
+                  <p>{destination.from}</p>
+                </>
+              )}
+              <br />
+              <p className="font-medium text-[#0a0a0a]">
+                Package Contains : <br />
+                {details}
+              </p>
+              <br />
+              <p className="font-medium text-[#0a0a0a]">Quantity : {qty}</p>
+              <p className="font-medium text-[#0a0a0a]">Status : </p>
+              {/* <p>{warehouses}</p> */}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
