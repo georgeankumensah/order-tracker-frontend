148d4402184965ea45e74a33662a7e43a6a25530
FosterSOAsare <fostersoasare@gmail.com>
2023-10-05 01:09:46 +0000

    Project progress

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,28 +1,25 @@
-import axios from "axios";
-import {useEffect,useState} from "react";
+import { useEffect } from "react";
 import api from "../api/api";
 
 const Dashboard = () => {
+	useEffect(() => {
+		const fetchAllOrders = async () => {
+			try {
+				await api.get("/orders").then((res) => {
+					console.log(res.data);
+				});
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchAllOrders();
+	}, []);
+	return (
+		<div>
+			<h1 className="text-3xl py-4">Dashboard</h1>
+			<hr />
+		</div>
+	);
+};
 
-    const fetchAllOrders = async () =>{
-        try {
-            await api.get("/orders").then(res=>{
-                console.log(res.data)
-            })
-        } catch (error) {
-            console.log(error)
-        }
-      
-    }
-    useEffect(
-      fetchAllOrders(),[]
-    )
-    return (
-        <div>
-            <h1 className="text-3xl py-4">Dashboard</h1>
-            <hr />
-        </div>
-    )
-}
-
-export default Dashboard
+export default Dashboard;
--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -6,7 +6,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import deliveryGuy from "../assets/delivery-guy.png";
-import spinner from "../assets/spinner.gif"
+import spinner from "../assets/spinner.gif";
 
 import { selectedProduct } from "../redux/actions/productActions";
 import ProductComponent from "./ProductComponent";
@@ -14,114 +14,94 @@ import ProductComponent from "./ProductComponent";
 import { getTrackingDetails } from "../../utils";
 
 const ProductListing = () => {
-  const dispatch = useDispatch();
-  const [trackingId, setTrackingId] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const url = "https://goldordertracker.onrender.com";
-  const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const [trackingId, setTrackingId] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const url = "http://localhost:8000";
+	const navigate = useNavigate();
 
-  const Loader = () =>{
-    return(
-      <div className="flex items-center  ">
-        <img src={spinner} className="w-[2rem] aspect-square" alt="spinner" />
-        <p className="font-medium ml-2">
-        Wait while we find your package..
-        </p>
-      </div>
-    )
-  }
+	const Loader = () => {
+		return (
+			<div className="flex items-center  ">
+				<img src={spinner} className="w-[2rem] aspect-square" alt="spinner" />
+				<p className="font-medium ml-2">Wait while we find your package..</p>
+			</div>
+		);
+	};
 
-  const products = useSelector((state) => state);
-  console.log(products);
-  const handleTrackOrder = async () => {
-    console.log("trigger");
-    setIsLoading(true);
-    // H77ca4fJh
+	const products = useSelector((state) => state);
+	console.log(products);
+	const handleTrackOrder = async () => {
+		console.log("trigger");
+		setIsLoading(true);
+		// H77ca4fJh
 
-    await axios
-      .get(url + "/orders/shareable/" + trackingId)
-      .then((res) => {
-        console.log(res.data);
-        setIsLoading(false);
-        navigate(`/packages/${trackingId}`);
-        dispatch(selectedProduct(res.data));
-        return res.data;
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        const status = err.response.status;
-        if (status == 404 ){
-          toast.error("Tracking ID not found", {
-            position: "bottom-left",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",})
-        }else{
-        toast.error(err.message, {
-          position: "bottom-left",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });}
-        console.log(err);
-        setTrackingId("")
-        // return err
-      });
-  };
+		await axios
+			.get(url + "/orders/shareable/" + trackingId)
+			.then((res) => {
+				console.log(res.data);
+				setIsLoading(false);
+				navigate(`/packages/${trackingId}`);
+				dispatch(selectedProduct(res.data));
+				return res.data;
+			})
+			.catch((err) => {
+				setIsLoading(false);
+				const status = err.response.status;
+				if (status == 404) {
+					toast.error("Tracking ID not found", {
+						position: "bottom-left",
+						autoClose: 5000,
+						hideProgressBar: false,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+						theme: "light",
+					});
+				} else {
+					toast.error(err.message, {
+						position: "bottom-left",
+						autoClose: 5000,
+						hideProgressBar: false,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+						theme: "light",
+					});
+				}
+				console.log(err);
+				setTrackingId("");
+				// return err
+			});
+	};
 
-  return (
-    <div className=" bg-[#f7f7f7]">
-      <ToastContainer
-        position="bottom-left"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-      <div className="flex items-center justify-center">
-        <div className="flex flex-col pl-[2rem] gap-[1rem] relative">
-          <h1 className="text-[#003C75] text-[4.40356rem] not-italic font-extrabold leading-[normal]">
-            QUICKLY TRACK YOUR PACKAGE
-          </h1>
-          <div className="flex items-center border-[1px] w-fit shadow-lg">
-            <input
-              type="text"
-              required
-              className="w-[20rem] h-[3rem] bg-white p-[0.5rem] outline-none"
-              placeholder="Tracking ID"
-              value={trackingId}
-              onChange={(e) => setTrackingId(e.target.value)}
-            />
-            <button
-              onClick={() => handleTrackOrder()}
-              className="bg-green-600 h-[3rem] w-[8rem] text-xl font-bold text-white px-[15px]"
-            >
-              Track
-            </button>
-          </div>
-        {isLoading  && <Loader/>}
-        </div>
-        <img
-          src={deliveryGuy}
-          className="aspect-auto h-[34.625rem] bg-yellow-200"
-          alt="delivery guy"
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className=" bg-[#f7f7f7]">
+			<ToastContainer position="bottom-left" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
+			<div className="flex items-center justify-center">
+				<div className="flex flex-col pl-[2rem] gap-[1rem] relative">
+					<h1 className="text-[#003C75] text-[4.40356rem] not-italic font-extrabold leading-[normal]">QUICKLY TRACK YOUR PACKAGE</h1>
+					<div className="flex items-center border-[1px] w-fit shadow-lg">
+						<input
+							type="text"
+							required
+							className="w-[20rem] h-[3rem] bg-white p-[0.5rem] outline-none"
+							placeholder="Tracking ID"
+							value={trackingId}
+							onChange={(e) => setTrackingId(e.target.value)}
+						/>
+						<button onClick={() => handleTrackOrder()} className="bg-green-600 h-[3rem] w-[8rem] text-xl font-bold text-white px-[15px]">
+							Track
+						</button>
+					</div>
+					{isLoading && <Loader />}
+				</div>
+				<img src={deliveryGuy} className="aspect-auto h-[34.625rem] bg-yellow-200" alt="delivery guy" />
+			</div>
+		</div>
+	);
 };
 
 export default ProductListing;
